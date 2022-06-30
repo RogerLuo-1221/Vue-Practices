6aafe0c6c1147dfcb5d9458dd9ecbb86a0a2a03e
RogerLuo-1221 <rluo01@wesleyan.edu>
2022-06-30 17:13:26 -0400

    Add Num Counter

---
+++ Num Counter/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue'
+
+const num = ref(0)
+
+function add() {
+  if (num.value < 10){
+    num.value++
+  }else{
+    alert('cant be bigger')
+  }
+}
+
+function sub() {
+  if (num.value > 0){
+    num.value--
+  }else{
+    alert('cant be smaller')
+  }
+}
+</script>
+
+<template>
+  <div id="app">
+    <div class="input-num">
+      <button @click="sub()">-</button>
+    <span>{{ num }}</span>
+    <button @click="add()">+</button>
+    </div>
+  </div>
+</template>
+
+<style>
+  @import './assets/style.css';
+</style>
